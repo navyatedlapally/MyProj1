34d8a055c1bbba8ba0ab2bf4de82dc676490d784
navyatedlapally <74097584+navyatedlapally@users.noreply.github.com>
2020-11-07 16:38:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('form');
+const button = form.querySelector('[type="submit"]');
+const msg = 'please provide a valid email address';
+
+const error = document.createElement('p');
+error.classList.add('error_text');
+error.textContent = msg;
+
+function checkEmail(e) {
+  e.preventDefault();
+  const email = e.target.querySelector('[type="email]').value;
+  if(!validateEmail(email)) {
+    form.classList.add('error');
+    form.insertBefore(error, button)
+  }else {
+    form.classList.remove('error');
+    form.removeChild(error);
+  }
+  console.log(email);
+}
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+form.addEventListener('submit', checkEmail);
